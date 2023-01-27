05b867711487e8d38aa795485a946cac3638f976
OneWinterSigh <cabelloruizgerman@gmail.com>
2023-01-27 19:43:55 +0100

    asdfl

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// View -> UIView
+
 export default function App() {
+console.log("app executed");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello, hola!</Text>
       <StatusBar style="auto" />
     </View>
   );
